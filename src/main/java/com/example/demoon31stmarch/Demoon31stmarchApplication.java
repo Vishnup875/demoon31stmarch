f07fa8b0e022ee58c553678a57559e04a1c3726c
Vishnup875 <wdvest1995@gmail.com>
2023-04-01 22:42:33 +0530

    democode

---
+++ src/main/java/com/example/demoon31stmarch/Demoon31stmarchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoon31stmarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoon31stmarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoon31stmarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoon31stmarch/Demoon31stmarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoon31stmarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoon31stmarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
